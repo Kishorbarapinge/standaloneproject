fe6b83b570a6582f6959c6b90deae37ca0e11adc
kishor <kishor@gmail.com>
2024-03-03 16:25:42 +0530

    standalone project

---
+++ src/main/java/com/standalonproject/App.java
@@ -0,0 +1,13 @@
+package com.standalonproject;
+
+/**
+ * Hello prajakta bhakte!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
